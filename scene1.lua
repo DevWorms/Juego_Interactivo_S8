721d04cd4e3fbdd72f0c46fa488350c9126401ad
Gus Gama <beattguss@gmail.com>
2017-12-06 09:43:37 -0600

    voy a clase y termino esto a las 10
    no entendi bien lo del timer

---
+++ scene1.lua
@@ -0,0 +1,125 @@
+---------------------------------------------------------------------------------
+--
+-- scene.lua
+--
+---------------------------------------------------------------------------------
+
+local sceneName = ...
+
+local composer = require( "composer" )
+
+-- Load scene with same root filename as this file
+local scene = composer.newScene( sceneName )
+local background
+local btnS8
+
+---------------------------------------------------------------------------------
+
+local nextSceneButton
+local contador = 1
+local function btnTap(event)
+    
+    --utils.tipoImagen()
+    --utils.reproducirSonido("boton")
+    --composer.gotoScene ( event.target.destination, { effect = "fade"} )
+    
+    print(contador)
+    print("cambio la imagen")
+    
+    return true
+    end 
+local function cambiaImagen(event)
+    contador = contador+1
+    background = display.newImage("images/imagen"..contador..".jpg" )
+    background:translate( display.contentWidth/2, display.contentHeight/2 )
+    print (contador)
+end
+function scene:create( event )
+    local sceneGroup = self.view
+    
+    btnS8= display.newImage(sceneGroup, "images/s8.png")
+        btnS8:translate( display.contentWidth - 95,display.contentHeight - 35)
+        
+
+    -- Called when the scene's view does not exist
+    -- 
+    -- INSERT code here to initialize the scene
+    -- e.g. add display objects to 'sceneGroup', add touch listeners, etc
+end
+
+function scene:show( event )
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if phase == "will" then
+        -- Called when the scene is still off screen and is about to move on screen
+        local title = self:getObjectByName( "Title" )
+        title.x = display.contentWidth / 2
+        title.y = display.contentHeight / 2
+        title.size = display.contentWidth / 10
+        background = display.newImage("images/imagen"..contador..".jpg" )
+        btnS8:addEventListener("tap", cambiaImagen)
+
+
+        
+    elseif phase == "did" then
+        -- Called when the scene is now on screen
+        -- 
+        -- INSERT code here to make the scene come alive
+        -- e.g. start timers, begin animation, play audio, etc
+        
+        -- we obtain the object by id from the scene's object hierarchy
+        nextSceneButton = self:getObjectByName( "GoToScene2Btn" )
+        if nextSceneButton then
+        	-- touch listener for the button
+        	function nextSceneButton:touch ( event )
+        		local phase = event.phase
+        		if "ended" == phase then
+        			composer.gotoScene( "scene2", { effect = "fade", time = 300 } )
+        		end
+        	end
+        	-- add the touch event listener to the button
+        	nextSceneButton:addEventListener( "touch", nextSceneButton )
+        end
+        
+    end 
+end
+
+function scene:hide( event )
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if event.phase == "will" then
+        -- Called when the scene is on screen and is about to move off screen
+        --
+        -- INSERT code here to pause the scene
+        -- e.g. stop timers, stop animation, unload sounds, etc.)
+    elseif phase == "did" then
+        -- Called when the scene is now off screen
+		if nextSceneButton then
+			nextSceneButton:removeEventListener( "touch", nextSceneButton )
+		end
+    end 
+end
+
+
+function scene:destroy( event )
+    local sceneGroup = self.view
+
+    -- Called prior to the removal of scene's "view" (sceneGroup)
+    -- 
+    -- INSERT code here to cleanup the scene
+    -- e.g. remove display objects, remove touch listeners, save state, etc
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+---------------------------------------------------------------------------------
+
+return scene
